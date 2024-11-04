a582e99fda72bd90848739c3fb011a44f785d7bc
EdoardoFerrini <edoardo.ferrini@gmail.com>
2024-11-04 09:54:47 +0100

    add conditional rendering and css

--- src/App.js
+++ src/App.js
@@ -5,19 +5,34 @@ function App() {
     { id: 1, name: "Go to the supermarket", completed: true },
     { id: 2, name: "Study programming", completed: false },
     { id: 3, name: "Go out for a walk", completed: true },
-    { id: 4, name: "Watch a movie", completed: false }
+    { id: 4, name: "Watch a movie", completed: false },
   ]);
+
+  const [show, setShow] = useState(true);
+
+  function handleDelete(id) {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
 
   return (
     <div className="App">
       <h1>Todo</h1>
       <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <span>{task.name}</span>
-            <button className="delete">Delete</button>
-          </li>
-        ))}
+        <button class="trigger" onClick={() => setShow(!show)}>
+          Toggle
+        </button>
+        {show &&
+          tasks.map((task) => (
+            <li
+              key={task.id}
+              className={task.completed ? "completed" : "incomplete"}
+            >
+              <span>{task.name}</span>
+              <button className="delete" onClick={() => handleDelete(task.id)}>
+                Delete
+              </button>
+            </li>
+          ))}
       </ul>
     </div>
   );
