fe13b527d7a3b5c2e1bb4082c1de983c10ae8ba7
EdoardoFerrini <edoardo.ferrini@gmail.com>
2024-11-04 09:45:03 +0100

    add task and css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, name: "Go to the supermarket", completed: true },
+    { id: 2, name: "Study programming", completed: false },
+    { id: 3, name: "Go out for a walk", completed: true },
+    { id: 4, name: "Watch a movie", completed: false }
+  ]);
+
   return (
     <div className="App">
       <h1>Todo</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <span>{task.name}</span>
+            <button className="delete">Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
